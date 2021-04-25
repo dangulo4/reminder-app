7d1965c06643d613a6d40c49977863841aa41077
Daniel Angulo <dangulo4@gmail.com>
2021-04-24 19:48:35 -0700

    added profile model

---
+++ client/build/precache-manifest.bc36830be2e3cc09ce6eb7e3919e7d1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b278a53df1f1d2648c4",
+    "url": "/static/css/main.99c35337.chunk.css"
+  },
+  {
+    "revision": "3b278a53df1f1d2648c4",
+    "url": "/static/js/main.5f470c3a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0fb24ff09dfe4b7d5fb0",
+    "url": "/static/js/2.7f6119f3.chunk.js"
+  },
+  {
+    "revision": "92fa0e1cd9daa63291fa637c7c92eda8",
+    "url": "/index.html"
+  }
+];
