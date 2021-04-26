a1ca7437fadf85a78240a9ff804f08839947937d
Daniel Angulo <dangulo4@gmail.com>
2021-04-25 17:05:48 -0700

    added profile create and update routes

--- models/Profile.js
+++ models/Profile.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const ProfileSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
@@ -8,99 +9,102 @@ const ProfileSchema = new mongoose.Schema({
     type: String,
   },
   website: {
-    location: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+      company: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true,
+      },
+      degree: {
+        type: String,
+        required: true,
+      },
+      fieldofstudy: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
       type: String,
     },
-    status: {
-      type: String,
-      required: true,
-    },
-    skills: {
-      type: [String],
-    },
-    bio: {
+    twitter: {
       type: String,
     },
-    githubusername: {
+    facebook: {
       type: String,
     },
-    experience: [
-      {
-        title: {
-          type: String,
-          required: true,
-        },
-        company: {
-          type: String,
-          required: true,
-        },
-        location: {
-          type: String,
-        },
-        from: {
-          type: Date,
-          required: true,
-        },
-        to: {
-          type: Date,
-        },
-        current: {
-          type: Boolean,
-          default: false,
-        },
-        description: {
-          type: String,
-        },
-      },
-    ],
-    education: [
-      {
-        school: {
-          type: String,
-          required: true,
-        },
-        degree: {
-          type: String,
-          required: true,
-        },
-        fieldofstudy: {
-          type: String,
-          required: true,
-        },
-        from: {
-          type: Date,
-          required: true,
-        },
-        to: {
-          type: Date,
-        },
-        current: {
-          type: Boolean,
-          default: false,
-        },
-        description: {
-          type: String,
-        },
-      },
-    ],
-    social: {
-      youtube: {
-        type: String,
-      },
-      twitter: {
-        type: String,
-      },
-      facebook: {
-        type: String,
-      },
-      linkedin: {
-        type: String,
-      },
-      instagram: {
-        type: String,
-      },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
     },
   },
+
   date: {
     type: Date,
     default: Date.now,
