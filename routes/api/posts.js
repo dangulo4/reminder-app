622d62702259b1d14a3982c837ee7831e4cbd335
Daniel Angulo <dangulo4@gmail.com>
2021-04-28 12:33:01 -0700

    added route to unlike post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -119,7 +119,38 @@ router.put('/like/:id', auth, async (req, res) => {
       return res.status(400).json({ msg: 'This post is already liked' });
     }
     post.likes.unshift({ user: req.user.id });
+
     await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
+//@route  PUT api/posts/like/:id
+//@desc   Unlike post by ID
+//@access Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //check if the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+    // Get remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+    // splice the array
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+
     res.json(post.likes);
   } catch (err) {
     console.error(err.message);
