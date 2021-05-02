b7bc072c57372d2424ac2e213c48ea72dbcc2390
Daniel Angulo <dangulo4@gmail.com>
2021-05-01 19:01:05 -0700

    added logout and navbar auth

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,6 +5,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from '../actions/types';
 
 // create variable to capture state
@@ -39,6 +40,7 @@ export default function (state = initialState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       // remove key value from  local storage
       localStorage.removeItem('token');
       return {
