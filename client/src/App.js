425560299181bb72e13726169bb60f5f1cdb0e5b
Daniel Angulo <dangulo4@gmail.com>
2021-06-02 20:17:54 -0700

    added post reducer

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Posts from './components/posts/Posts';
 import Profile from './components/profile/Profile';
 import PrivateRoute from './components/routing/PrivateRoute';
 
@@ -69,6 +70,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               ></PrivateRoute>
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
