05a7f0460cf296f25b827e361ade33ea3da7769b
Daniel Angulo <dangulo4@gmail.com>
2021-09-07 20:49:04 -0700

    build command

---
+++ client/build/precache-manifest.7aabad7ad8988f7a45557de1b62662f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4297c60143a459e2946f",
+    "url": "/static/css/main.c932cbe5.chunk.css"
+  },
+  {
+    "revision": "4297c60143a459e2946f",
+    "url": "/static/js/main.2ea0a9ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c7ac5ad2ed7dfacdd7e",
+    "url": "/static/js/2.abbb032c.chunk.js"
+  },
+  {
+    "revision": "2c9e091d7d9d41da8f8e166b24538f87",
+    "url": "/static/media/showcase.2c9e091d.jpg"
+  },
+  {
+    "revision": "36f729d204a8b3e9f1c43f41a09420f7",
+    "url": "/index.html"
+  }
+];
